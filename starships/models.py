0a9333207699e77d368836f01bf5cb2e41779a7a
simonsprofile <supersim65@gmail.com>
2023-12-04 18:36:21 +0000

    More verbose name correction for the Starship model.

--- starships/models.py
+++ starships/models.py
@@ -12,13 +12,20 @@ class Starship(models.Model):
     value_when_new = models.IntegerField('Value when new', null=False)
 
     length = models.IntegerField('Length', null=False)
-    max_atmosphering_speed = models.IntegerField('Maximum Speed', null=False)
+    max_atmosphering_speed = models.IntegerField(
+        'Maximum Atmosphering Speed',
+        null=False
+    )
     hyperdrive_capacity = models.IntegerField('Hyperdrive Capacity', null=False)
     min_crew = models.IntegerField('Minimum Crew Compliment', null=False)
     max_crew = models.IntegerField('Maximum Crew Compliment', null=False)
     passengers = models.IntegerField('Maximum Passenger Capacity', null=False)
     cargo_capacity = models.IntegerField('Maximum Cargo Capacity', null=False)
-    consumables_for = models.CharField('Name', max_length=100, blank=False)
+    consumables_for = models.CharField(
+        'Consumable Capacity For',
+        max_length=100,
+        blank=False
+    )
     mglt = models.IntegerField('Maximum Speed', null=False)
 
     def __str__(self):
